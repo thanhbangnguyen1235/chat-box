9ebb21b294f0a48c6d159646cda3b4849b10e07e
Nguyễn Thanh Bằng <bangnguyen1235@gmail.com>
2022-10-23 14:09:32 +0700

    2:09

--- src/Components/Messages/OwnerMessage.js
+++ src/Components/Messages/OwnerMessage.js
@@ -3,27 +3,12 @@ import React from "react";
 export default function OwnerMessage({ message }) {
   return (
     <>
-      {message.noidung ? (
-        <div className="d-flex flex-row justify-content-end mb-4">
-          <div className="p-3 me-3 border">
-            <p className="small mb-0">{message.noidung}</p>
-          </div>
-          <img src={message.noidung} alt="avatar 1" />
+      <div className="d-flex flex-row justify-content-end mb-4">
+        <div className="p-3 me-3 border">
+          <div dangerouslySetInnerHTML={{ __html: message.noidung }} />
         </div>
-      ) : (
-        <div className="d-flex flex-row justify-content-end mb-4">
-          <div className="me-3" style={{ borderRadius: "15px" }}>
-            <div className="bg-image">
-              <img src={message.noidung} style={{ borderRadius: "15px" }} />
-            </div>
-          </div>
-          <img
-            src={message.noidung}
-            alt="avatar 1"
-            style={{ width: "45px", height: "100%" }}
-          />
-        </div>
-      )}
+        <img src={message.noidung} alt="avatar 1" />
+      </div>
     </>
   );
 }
